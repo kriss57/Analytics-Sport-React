121823a4216fd9b5b012bb08ac6ece102724d727
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-03-24 19:04:42 +0100

    create folder and css user

---
+++ src/pages/User/User.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+//import { userService } from '../_services/user.service';
+import { mockService } from '../../_services/mock.service';
+
+import './user.css'
+
+const User = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        mockService.getMockUser()
+            .then(res => {
+                setUsers(res.data)
+                console.log(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div className='User'>
+            <h1>Choix de l'utilisateur</h1>
+            {users.map(user => (
+                <Link className='link' key={user.id} to={`/user/home/${user.id}`}>
+                    <p key={user.id}>{user.userInfos.firstName} {user.userInfos.lastName}</p>
+                </Link>
+            ))}
+        </div>
+    );
+};
+
+export default User;
