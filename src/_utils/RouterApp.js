4f65c6478711b4507219ccefeec3e1327bd994dd
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-03-24 18:03:26 +0100

    creation of the routing file and mock file

---
+++ src/_utils/RouterApp.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home/Home';
+import Layout from '../pages/Layout'
+import User from '../pages/User';
+
+const RouterApp = () => {
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route index element={<User />} />
+
+                <Route path='/user' element={<User />} />
+                <Route path='/user/home' element={<Home />} />
+            </Route>
+
+        </Routes>
+    );
+};
+
+export default RouterApp;
