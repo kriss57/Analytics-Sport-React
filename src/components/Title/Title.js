463d18fa8762225c7f4d0d98fa99c9c3dd27cec8
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-04-06 09:59:31 +0200

    cleaning, adding proptypes, jsdoc

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { userService } from '../../_services/user.service';
+import PropTypes from 'prop-types'
 
+/**
+ * 
+ * @param {string} uid
+ * @returns template
+ */
 const Title = ({ uid }) => {
-    console.log(uid);
+
     const [user, setUser] = useState({})
 
     useEffect(() => {
         userService.getUser(parseInt(uid))
             .then((res) => {
-                console.log(res)
                 setUser(res.data.data.userInfos)
             })
             .catch(err => console.log(err))
@@ -16,11 +21,15 @@ const Title = ({ uid }) => {
 
     return (
         <div className="Title">
-            <h1>Bonjour {user.firstName}</h1>
-            <p></p>
+            <h1 style={{ fontWeight: '500', fontSize: '48px' }}>Bonjour <span style={{ color: 'red' }} >{user.firstName}</span></h1>
+            <p style={{ marginBottom: '15px', fontSize: '18px' }}>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
         </div>
 
     )
 };
+
+Title.prototype = {
+    uid: PropTypes.string.isRequired
+}
 
 export default Title;
