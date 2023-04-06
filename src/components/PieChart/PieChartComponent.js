8aa022b1057dd82ecb34574ead0ae16c4730b11b
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-04-06 08:36:42 +0200

    add PieChart component

---
+++ src/components/PieChart/PieChartComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+import './pieChartComponent.css'
+
+const PieChartComponent = () => {
+
+    const data = [
+        { name: 'Group A', value: 1500 },
+
+    ];
+
+    return (
+        <div className='PieChartComponent'>
+            <h3 className='perf-title' >Score</h3>
+            <ResponsiveContainer width='100%' height={220}>
+                <PieChart width={800} height={400} >
+                    <Pie
+                        data={data}
+                        cx={100}
+                        cy={80}
+                        startAngle={70}
+                        endAngle={200}
+                        innerRadius={70}
+                        outerRadius={79}
+                        fill="red"
+                        dataKey="value"
+                        cornerRadius={15}
+                    >
+                        <Cell stroke='' fill='red' />
+                    </Pie>
+                </PieChart>
+
+            </ResponsiveContainer>
+            <div className="score"></div>
+
+        </div>
+    );
+};
+
+export default PieChartComponent;
